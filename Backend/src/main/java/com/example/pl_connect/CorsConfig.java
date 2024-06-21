35f5e81b4b3b02c64cbdec983c4aa1bc04c134f3
Erik Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2024-06-21 12:54:16 -0400

    Update CorsConfig.java

--- Backend/src/main/java/com/example/pl_connect/CorsConfig.java
+++ Backend/src/main/java/com/example/pl_connect/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "https://premierzone.netlify.app")
+                        .allowedOrigins("http://localhost:3000", "https://premierzone.vercel.app")
                         .allowedMethods("GET");
             }
         };
